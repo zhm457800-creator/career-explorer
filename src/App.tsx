c0412f592ce251e1e06473458ce845235499651b
Hm Z <zhm457800@gmail.com>
2026-04-02 16:25:43 +0800

    feat: 添加页面滚动到顶部功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
+import { ScrollToTop } from './components/ScrollToTop';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Archive } from './pages/Archive';
@@ -9,6 +10,7 @@ import { Extension } from './pages/Extension';
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
